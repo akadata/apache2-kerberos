c908b3e757309b738b716ceda777c86bb2bb43af
glegoux <gilles.legoux@gmail.com>
2018-06-14 22:53:27 +0200

    Add option to google chrome

--- google-chrome.sh
+++ google-chrome.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-google-chrome --auth-server-whitelist="*.example.com"
+google-chrome \
+  --auth-server-whitelist="10.5.0.4,http-service.example.com" \
+  --auth-negotiate-delegate-whitelist="10.5.0.4,http-service.example.com" \
+  &> /dev/null &
